b31b4677022e141c1280e73c47fafe7d513c13c0
daviffsilva <davif127@gmail.com>
2021-05-02 18:43:26 -0300

    v2.2
    Signed-off-by: daviffsilva <davif127@gmail.com>

--- index.js
+++ index.js
@@ -11,23 +11,15 @@ const downloadMods = function(mods, separator = ','){
 
 const downloadMod = function(modId){
     const command = `${CONFIG.STEAM_CMD_PATH} +login anonymous +force_install_dir ${CONFIG.TEMP_MODS_PATH} +workshop_download_item ${CONFIG.GAME_ID} ${modId} validate +quit`;
-//, command.split(' ').splice(1)
-    let commandRun = child_process.spawn(command.split(' ')[0]);
-
-    commandRun.stdout.on('data', function (data) {
-        console.log('stdout: ' + data.toString());
-    });
-
-    commandRun.stderr.on('data', function (data) {
-        console.log('stderr: ' + data.toString());
-    });
-
-    commandRun.on('exit', function (code) {
-        console.log('child process exited with code ' + code.toString());
+    console.log(command);
+    child_process.exec(command, (error, stdout, stderr) => {
+    // child_process.exec(`dir -a`, (error, stdout, stderr) => {
+        console.log('teste');
+        console.log(error, stderr, stdout);
     });
 }
 
-downloadMods('1673456286');
+downloadMods('463939057');
 
 // exec('dir', (error, stdout, stderr) => {
     
