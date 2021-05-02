9a46ab0f8c16df39e452c338d2ceb8becb46a0b4
daviffsilva <davif127@gmail.com>
2021-05-02 16:52:39 -0300

    v1
    Signed-off-by: daviffsilva <davif127@gmail.com>

---
+++ index.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process");
+const CONFIG = require("./config.json");
+
+const downloadMods = function(mods, separator = ','){
+    const modIds = mods.split(separator);
+
+    modIds.forEach(modId => {
+        downloadMod();
+    });
+}
+
+const downloadMod = function(modId){
+    exec(`${CONFIG.STEAM_CMD_PATH} +login anonymous +force_install_dir ${CONFIG.TEMP_MODS_PATH} +workshop_download_item ${CONFIG.GAME_ID} ${modId} validate`, (error, stdout, stderr) => {
+        console.log(error, stderr, stdout);
+    });
+}
+
+downloadMods('1673456286');
+
+// exec('dir', (error, stdout, stderr) => {
+    
+// });
